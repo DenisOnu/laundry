f235240b7fd2f721a7c3d69b8406f1de13d01c2c
Denis Onu <Denisonu7@gmail.com>
2022-05-18 13:13:12 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+//Get the move to top button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 40px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
